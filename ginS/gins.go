723a04c7c8257295c94f77c871a2bc28d56bc69e
Vladislav Sukhin <vsukhin@envisionworld.co>
2020-10-13 16:02:37 +0300

    Rename gin

--- ginS/gins.go
+++ ginS/gins.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/gin-gonic/gin"
+	"github.com/fixbanking/gin"
 )
 
 var once sync.Once
--- render/json.go
+++ render/json.go
@@ -10,8 +10,8 @@ import (
 	"html/template"
 	"net/http"
 
-	"github.com/gin-gonic/gin/internal/bytesconv"
-	"github.com/gin-gonic/gin/internal/json"
+	"github.com/fixbanking/gin/internal/bytesconv"
+	"github.com/fixbanking/gin/internal/json"
 )
 
 // JSON contains the given interface object.
